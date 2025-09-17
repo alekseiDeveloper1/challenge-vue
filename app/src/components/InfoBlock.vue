b5937db4983c32929cf81f3da1334242e0a8c487
Aleksey <Aleks_266@mail.ru>
2025-09-17 19:57:27 +0300

    feat: HTML coding

---
+++ app/src/components/InfoBlock.vue
@@ -0,0 +1,12 @@
+<template>
+    <q-banner class="bg-grey-3">
+      <template v-slot:avatar>
+        <q-icon name="warning" color="primary" />
+      </template>
+      Для указания нескольких меток для одной пары логин/пароль используйте разделитель ;
+    </q-banner>
+</template>
+
+<script setup lang="ts">
+
+</script>
--- app/src/pages/IndexPage.vue
+++ app/src/pages/IndexPage.vue
@@ -1,43 +1,66 @@
 <template>
-  <q-page class="row items-center justify-evenly">
-    <example-component
-      title="Example component"
-      active
-      :todos="todos"
-      :meta="meta"
-    ></example-component>
+  <q-page class="q-pa-md">
+    <q-card class="my-card" flat>
+      <q-card-section>
+        <div class="text-h6 q-pa-md">
+          Учетные записи
+          <q-btn label="+" outline color="gray" />
+        </div>
+        <div class="text-subtitle2">
+          <info-block/>
+        </div>
+      </q-card-section>
+
+      <q-markup-table>
+        <thead>
+        <tr>
+          <th class="text-left">Метки</th>
+          <th class="text-left">Тип записи</th>
+          <th class="text-left">Логин</th>
+          <th class="text-left">Пароль</th>
+        </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td class="text-left">
+              <q-input v-model="mask" outlined/>
+            </td>
+            <td class="text-left">
+              <q-select v-model="mask" outlined/>
+            </td>
+            <td class="text-left">
+              <q-input v-model="mask" outlined/>
+            </td>
+            <td class="text-left">
+              <q-input v-model="password" outlined :type="isPwd ? 'password' : 'text'">
+                <template v-slot:append>
+                  <q-icon
+                    :name="isPwd ? 'visibility_off' : 'visibility'"
+                    class="cursor-pointer"
+                    @click="isPwd = !isPwd"
+                  />
+                </template>
+              </q-input>
+            </td>
+          </tr>
+        </tbody>
+      </q-markup-table>
+    </q-card>
+    <div class="text-h6">
+
+    </div>
+
+    <div class="row">
+
+    </div>
   </q-page>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
-import type { Todo, Meta } from 'components/models';
-import ExampleComponent from 'components/ExampleComponent.vue';
 
-const todos = ref<Todo[]>([
-  {
-    id: 1,
-    content: 'ct1',
-  },
-  {
-    id: 2,
-    content: 'ct2',
-  },
-  {
-    id: 3,
-    content: 'ct3',
-  },
-  {
-    id: 4,
-    content: 'ct4',
-  },
-  {
-    id: 5,
-    content: 'ct5',
-  },
-]);
-
-const meta = ref<Meta>({
-  totalCount: 1200,
-});
+import InfoBlock from "components/InfoBlock.vue";
+import {ref} from "vue";
+const mask = ref('')
+const password = ref('')
+const isPwd = ref(true)
 </script>
